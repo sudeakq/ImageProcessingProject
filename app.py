0aa078b5df26c3de1f9e1f97ed9c6e5c9b47e213
sudeakq <sudeakq@gmail.com>
2024-05-05 23:25:31 +0300

    blur filter added

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import numpy as np
 img_path = "images/1_xBb9dtIT9N4QmZInvHtUWg.webp"
 img = cv2.imread(img_path)
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-plt.imshow(gray_image)
+(thresh, output2) = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)
+output2 = cv2.GaussianBlur(output2, (5, 5), 3)
+output2 = cv2.Canny(output2, 180, 255)
+plt.imshow(output2)
 plt.show()
-print(gray_image.shape)
